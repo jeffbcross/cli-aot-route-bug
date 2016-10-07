85bd5c6caec557344961a181987c797598d570d2
Jeff Cross <middlefloor@gmail.com>
2016-10-07 13:41:10 -0700

    add router and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
+import { HomeComponent } from './home/home.component';
+
+const homeRoute: Route = {
+  path: '',
+  component: HomeComponent
+};
+
+const otherRoute: Route = {
+  path: 'other',
+  component: ItemsComponent
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'items',
+        component: ItemsComponent
+      },
+      homeRoute,
+      otherRoute])
   ],
   providers: [],
   bootstrap: [AppComponent]
